c0d1fb0114eb8f9edd66d26314b054653a487d63
Carolina <98135902+CarolinaMV@users.noreply.github.com>
2022-03-09 23:23:32 -0500

    Delete Servlet directory

--- Servlet/src/main/webapp/validation.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    var x;
-    x = document.getElementById("id").value;
-    if (isNaN(x)) {
-        alert("Valor no numerico: '"+x+"'");
-        return false;
-    }
-    return true;
-}
